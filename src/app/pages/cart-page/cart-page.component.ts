9a3c587672063cc56525b527d93125d8dd61ab28
unknown <samaelmenshawy@gmail.com>
2024-10-19 23:01:51 +0200

    (cart page_checko out page)menna mohamed (check out page)menna elhawy-(notfound page)sondos

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,13 +1,75 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+interface IProduct {
+  image: '';
+  title: '';
+  category: '';
+  description: '';
+  price: 0;
+  id: 0;
+  qty: 1;
+}
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './cart-page.component.html',
-  styleUrl: './cart-page.component.css'
+  styleUrl: './cart-page.component.css',
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cart: IProduct[] = [];
 
+  total: any = 0;
+  constructor() {}
+  ngOnInit(): void {
+    const cart = localStorage.getItem('cart') || '';
+    const paresedData = JSON.parse(cart);
+    if (paresedData) {
+      this.cart = paresedData;
+      this.getcarttotal();
+    }
+  }
+  addamount(index: number) {
+    if (index < 0 || index >= this.cart.length) {
+      console.error('Invalid index');
+      return;
+    }
+
+    this.cart[index].qty++;
+    this.getcarttotal();
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  minusamount(index: number) {
+    if (index < 0 || index >= this.cart.length) {
+      console.error('Invalid index');
+      return;
+    }
+
+    if (this.cart[index].qty > 0) {
+      this.cart[index].qty--;
+      this.getcarttotal();
+      localStorage.setItem('cart', JSON.stringify(this.cart));
+    } else {
+      console.warn('Quantity cannot be less than zero.');
+    }
+  }
+  delamount(index: number) {
+    this.cart.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+    this.getcarttotal();
+  }
+  clearcart() {
+    this.cart = [];
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+    this.getcarttotal();
+  }
+  getcarttotal() {
+    this.total = 0;
+    for (let x in this.cart) {
+      this.total += Math.ceil(this.cart[x].price * this.cart[x].qty);
+    }
+  }
 }
--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -1,12 +1,85 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+interface IProduct {
+  image: '';
+  title: '';
+  category: '';
+  description: '';
+  price: 0;
+  id: 0;
+  qty: 1;
+}
 
 @Component({
   selector: 'app-checkout-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './checkout-page.component.html',
-  styleUrl: './checkout-page.component.css'
+  styleUrl: './checkout-page.component.css',
 })
-export class CheckoutPageComponent {
+export class CheckoutPageComponent implements OnInit {
+  @Input() firstname = '';
+  @Input() lastname = '';
+  @Input() phone: string = '';
+  @Input() email = '';
+  @Input() address = '';
+  selectedCity = '';
+  house = '';
+  postalCode = '';
+  zip = '';
+  saveAddress = false;
 
+  check: any[] = [];
+  cart: IProduct[] = [];
+  message: string = '';
+
+  total: any = 0;
+  constructor() {}
+  ngOnInit(): void {
+    const cart = localStorage.getItem('cart') || '';
+    const paresedData = JSON.parse(cart);
+    if (paresedData) {
+      this.cart = paresedData;
+      this.getcarttotal();
+    }
+  }
+  getcarttotal() {
+    this.total = 0;
+    for (let x in this.cart) {
+      this.total += Math.ceil(this.cart[x].price * this.cart[x].qty);
+    }
+  }
+  submit() {
+    if (
+      this.firstname &&
+      this.lastname &&
+      this.phone &&
+      this.email &&
+      this.address &&
+      this.selectedCity
+    ) {
+      this.check = [
+        {
+          firstname: this.firstname,
+          lastname: this.lastname,
+          phone: this.phone,
+          email: this.email,
+          address: this.address,
+          city: this.selectedCity,
+          house: this.house,
+          postalCode: this.postalCode,
+          zip: this.zip,
+          saveAddress: this.saveAddress,
+          totals: this.total,
+        },
+      ];
+
+      localStorage.setItem('checkoutData', JSON.stringify(this.check));
+      this.message = 'Checkout data saved successfully!'; // Success message
+    } else {
+      this.message = 'Please fill all required fields.'; // Error message
+    }
+  }
 }
